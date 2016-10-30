1e8b33f669c716c499888c4bba031cad143fe52f
s384276 <kinga1642@gmail.com>
2016-10-30 19:27:27 +0100

    Update Meal.py
    Dodałam nowe potrawy i coś tam pozmieniałam w metodach :x

--- Meal.py
+++ Meal.py
@@ -1,9 +1,8 @@
 class Meal(object):
 
-    listOfIngredients = []
-
     def __init__(self, name=None):
         self._name = name
+        self.listOfIngredients = []
 
     @property
     def name(self):
@@ -18,21 +17,63 @@ class Meal(object):
 
     def isIngredient(self, x):
         if x in self.listOfIngredients:
-            print("Jest skladnikiem")
+            return True
         else:
-            print("Nie jest skladnikiem")
+            return False
 
+    def printIngredients(self):
+        for x in self.listOfIngredients:
+            print(x)
 
+ListOfDishes = []
 Jajecznica = Meal("Jajecznica")
-Jajecznica.addIngredients(["Jajko", "Maslo", "Cebula", "Sol", "Pieprz"])
+Jajecznica.addIngredients(["Jajko", "Masło", "Cebula"])
+ListOfDishes.append(Jajecznica)
 
 Omlet = Meal("Jajecznica")
-Omlet.addIngredients(["Jajko", "Maslo", "Mleko", "Sol", "Pieprz"])
+Omlet.addIngredients(["Jajko", "Masło", "Mleko"])
+ListOfDishes.append(Omlet)
 
 Rosol = Meal("Rosol")
-Rosol.addIngredients(["Uda", "Korpus", "Skrzydelka", "Serca", "Zoladek", "Wloszczyzna", "Wolowina", "Woda", "Ziele angielskie", "Lisc", "Sol", "Pieprz"])
+Rosol.addIngredients(["Drób", "Włoszczyzna", "Wołowina"])
+ListOfDishes.append(Rosol)
 
-#wyszukiwanie czy jakis skladnik jst w potrawie:
+Tatar = Meal("Tatar")
+Tatar.addIngredients(["Wołowina", "Jajko", "Olej", "Cebula", "Ogórek"])
+ListOfDishes.append(Tatar)
 
-Rosol.isIngredient("Wolowina")
-Rosol.isIngredient("Szynka")
+Carpaccio = Meal("Carpaccio wołowe z rukolą i parmezanem")
+Carpaccio.addIngredients(["Wołowina", "Rukola", "Parmezan"])
+ListOfDishes.append(Carpaccio)
+
+Barszcz = Meal("Barszcz czerwony z kołdunami")
+Barszcz.addIngredients(["Bulion", "Burak", "Grzyby", "Kołduny"])
+ListOfDishes.append(Barszcz)
+
+Pierogi1 = Meal("Pierogi ze szpinakiem, suszonymi pomidorami i fetą")
+Pierogi1.addIngredients(["Szpinak", "Pomidor", "Feta"])
+ListOfDishes.append(Pierogi1)
+
+Pierogi2 = Meal("Pierogi z cielęciną i borowikami z masłem szałwiowym")
+Pierogi2.addIngredients(["Cielęcina", "Borowik", "Masło", "Szałwia"])
+ListOfDishes.append(Pierogi2)
+
+Zurek = Meal("Żurek z białą kiełbasą i jajkiem")
+Zurek.addIngredients(["Bulion", "Zakwas", "Kiełbasa", "Jajko"])
+ListOfDishes.append(Zurek)
+
+Krem1 = Meal("Krem z dyni z mlekiem kokosowym")
+Zurek.addIngredients(["Bulion", "Dynia", "Mleko kokosowe", "Śmietana"])
+ListOfDishes.append(Krem1)
+
+Krem2 = Meal("Krem z trufli z grzankami")
+Krem2.addIngredients(["Bulion", "Trufle", "Śmietana", "Grzanki"])
+ListOfDishes.append(Krem2)
+
+Krem3 = Meal("Krem z pomidorów")
+Krem3.addIngredients(["Bulion", "Pomidor", "Śmietana"])
+ListOfDishes.append(Krem3)
+
+Zupa = Meal("Zupa pomidorowa")
+Zupa.addIngredients(["Bulion", "Pomidor", "Śmietana"])
+ListOfDishes.append(Zupa)

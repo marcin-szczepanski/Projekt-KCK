f6d9564bc58f4ab06619a6074837e4bad16e163c
marcin-szczepanski <marcin.szczepanski1995@gmail.com>
2017-01-04 15:27:34 +0100

    Modify logic.py and add synonymous_words.txt

--- logic.py
+++ logic.py
@@ -1,45 +1,20 @@
-input = raw_input()
-expression = input.split()
-action = expression[0]
-length_of_expression = len(expression)
+import re
+import linecache
 
-class Term(object):
+def whataction(word = '', file = "synonymous_words.txt" ):
+    f = open(file, mode="r+")
+    count = len(f.readlines())+1
+    action = ''
+    for i in range(count):
+	    wiersz = linecache.getline(file, i).split(";")
+	    if word in wiersz:
+		    action=wiersz[-1]
+		    break;
+    f.close()
+    if action == '':
+	    action = "error"
+    return(action)
 
-	def __init__(self, numeral=None, noun=None, adjective=None):
-		self._numeral
-        self._noun = noun
-        self._adejctive = adjective
+###test
 
-    @property
-    def numeral(self):
-        return self._numeral
-
-    @numeral.setter
-    def numeral(self, value):
-        self._numeral = value
-		
-	@property
-    def noun(self):
-        return self._noun
-
-    @noun.setter
-    def noun(self, value):
-        self._noun = value
-		
-	@property
-    def adjective(self):
-        return self._adjective
-
-    @noun.setter
-    def adjective(self, value):
-        self._adjective = value
-
-counter = 1
-list_of_expressions = []
-list_of_terms = []
-while counter < length_of_expression:
-	list_of_terms[counter] = Term(expression[counter],expression[counter+1],expression[counter+2])
-	list_of_expressions.append(list_of_terms[counter])
-	counter = counter + 3
-	
-# co zrobić, gdy przed rzeczownikiem nie ma liczebnika?
+print (whataction("poprosić"))

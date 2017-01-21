e11ccbf94b6a68af21040c3990e9f5c8daec15f6
s384276 <kinga1642@gmail.com>
2017-01-22 00:07:46 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -27,24 +27,49 @@ def fromMenutoList(): #wczytuje menu.txt i tworzy z niego listę
             MenuLista.append(line.rstrip('\n'))
     return MenuLista
 
-#list = ["Naleśniki z serem"]
-list = ["Naleśniki z mięsem"]
 
-def zamow(list): #na podstawie listy tworzy zamówienie
-    x = findMeal(list[0])
-    if(x!=False):
-        y = str(fromMenutoList()[x-1]).split(";",2)[1].lower()
-        global kwota
-        kwota = kwota + int((fromMenutoList()[x-1]).split(";",2)[-1])
-        print("Zamówiono " + y)
-    else:
-        print("Nie mamy takiego dania! Proszę zamówić coś innego")
+def menuOdmienionetoArray(): #wczytuje plik menuOdmienione i tworzy z niego listę
+    with open('menuOdmienione', 'r', encoding='utf-8') as f:
+        results = []
+        for line in f:
+            results.append(line.strip().split(';'))
+        return results
 
-def zabierz(x):
-    if(x.lower() == "menu"):
-        print("Zabrano menu ze stołu")
-    if(x.lower() == "talerze"):
-        print("Zabrano talerze")
+def zamowF(list): #na podstawie listy tworzy zamówienie
+    a = len(list)
+    tab = []
+    print("Zamówiono: ")
+    for i in range(0,a):
+        tab.append(findMeal(list[i][1]))
+        if(tab[i]!=False):
+            ilosc = list[i][0]
+            global kwota
+            kwota = kwota + int(ilosc)*int((fromMenutoList()[tab[i]-1]).split(";",2)[-1])
+            print(odmiana(list,i))
+        else:
+            print("Chcesz zamówić danie, którego nie ma w menu! Proszę zamów coś innego :)")
+
+
+def odmiana(list, i):  #służy do skomplikowania wyjścia, np. zamówiono 2 serniki ale już dla 5 sztuk mamy zamówiono 5 serników
+        ilosc = list[i][0]
+        inMenu = findMeal(list[i][1])
+        if(ilosc > 4):
+            return(str(ilosc) + menuOdmienionetoArray()[inMenu-1][4])
+        else:
+           return(menuOdmienionetoArray()[inMenu-1][ilosc-1])
+
+
+def zabierz(list):
+    if list[0][0] == "zabranie":
+        list[0][0] = list[1][0]
+    x = list[0][0]
+    print("Zabierz " + x)
+
+def przyniesF(list):
+    if list[0][0] == "przyniesienie":
+        list[0][0] = list[1][0]
+    x = list[0][0]
+    print("Przynieś " + x)
 
 def podejdz():
     print("Kelner podchodzi do stołu")
@@ -53,22 +78,16 @@ def odejdz():
     print("Kelner odchodzi od stołu")
 
 def polec():
-    x = random.randint(1,17)
+    x = random.randint(1,19)
     polecam = str(fromMenutoList()[x-1]).split(";",2)[1].lower()
     print("Polecam " + polecam + "!")
 
-zamow(list)
-#list = ["Omlet","Jajecznica","Pierogi", "Zupa"]
 
-#zamow(list)
-#print(kwota)
-#polec()
+def zaplac(): # funkcja do placenia, oblicza sume zamowienia
+        print("Kwota do zapłaty to " + str(kwota) + " zł.")
 
-def zaplac(x): # funkcja do placenia, oblicza sume zamowienia
-    if(x.lower() == "karta"):
-        print("Wybrano płatność kartą. Kwota do zapłaty to " + str(kwota) + " zł.")
-    if(x.lower() == "gotówka"):
-        print("Wybrano płatność gotówką. Kwota do zapłaty to " + str(kwota) + " zł.")
 
-#zaplac("KARTA")
-#zaplac("gotówka")
+
+list = [[20, "pierogi ruskie"],[1, "sałatka jarzynowa"],[3, "naleśniki z serem"],[1, "pomidor"],[4,"jabłecznik"]]
+zamowF(list)
+zaplac()

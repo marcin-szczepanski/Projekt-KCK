7e0f2076a5b56220219657385e1741e2f8d12d23
marcin-szczepanski <marcin.szczepanski1995@gmail.com>
2017-01-28 19:15:13 +0100

    Add functions from akcje.py to logic.py

--- logic.py
+++ logic.py
@@ -1,39 +1,95 @@
 ### koduje Marcin
 import linecache
 import clear
+import random
 
-### na samym dole uwagi do części Wojtka i Kingi!!!
+kwota = 0
 
-### funkcje do implementacji przez Łukasza i Kingę (Marcin przygotowuje i przekazuje argumenty w postaci listy):
+def findMeal(x): #funkcja do znajdywania w pliku menu.txt odpowiedniego posiłku, o który prosi klient
+    lista = []
+    isItFound = False
+    lines = 0
+    with open('Menu.txt', 'r', encoding='utf-8') as searchfile:
+        for line in searchfile:
+            lines = lines + 1
+            if x in line:
+                line = line[0:line.find(';')]
+                lista.append(line.rstrip('\n'))
+                break
+        a = len(lista)
+        if a>0:
+            return lines
+        else:
+            return False
 
-def zamowF(list): ### argumentem będzie lista obiektów postaci [liczebnik (int), string z nazwą dania], np. list=[[2, "zupa pomidorowa"],[1, "sałatka jarzynowa]]; uwaga na nazwę funkcji: zamowF !
-	print ("funkcja zamów")
-	
-def zaplac(): ### płatność (tylko) gotówką (Kinga z rozmowy i analizy ile rzeczy zamówiono i za ile wnioskuje jaka jest kwota zamówienia)
-	print ("funkcja zapłać")
-	
-def zabierz(list): ### argumentem będzie słowo, co zabrać (lista w pliku words.txt)
-	if list[0][0] == "zabranie":
-		list[0][0] = list[1][0]
-	x = list[0][0]
-	### po prostu wpiszcie te 3 linijki powyżej na początku funkcji w swoich częściach, żeby działało; ten x traktujcie jako argument funkcji zamiast samego list :)
-	print ("funkcja zabierz")
-	
-def przyniesF(list): ### argumentem będzie słowo, co zabrać (lista w pliku words.txt); uwaga na nazwę funkcji: przyniesF !
-	if list[0][0] == "przyniesienie":
-		list[0][0] = list[1][0]
-	x = list[0][0]
-	### po prostu wpiszcie te 3 linijki powyżej na początku funkcji w swoich częściach, żeby działało; ten x traktujcie jako argument funkcji zamiast samego list :)
-	print ("funkcja przynieś")
+def fromMenutoList(): #wczytuje menu.txt i tworzy z niego listę
+    MenuLista = []
+    with open('Menu.txt', 'r', encoding='utf-8') as searchfile:
+        for line in searchfile:
+            MenuLista.append(line.rstrip('\n'))
+    return MenuLista
 
-def polec(): ### brak argumentów; chodzi o polecenie czegoś losowo z menu
-	print ("funkcja poleć")
 
-def odejdz(): ### brak argumentów; chodzi o to, żeby kelner odszedł od stołu
-	print ("funkcja odejdź")
+def menuOdmienionetoArray(): #wczytuje plik menuOdmienione i tworzy z niego listę
+    with open('menuOdmienione.txt', 'r', encoding='utf-8') as f:
+        results = []
+        for line in f:
+            results.append(line.strip().split(';'))
+        return results
+
+def zamowF(list): #na podstawie listy tworzy zamówienie
+    a = len(list)
+    tab = []
+    dania_komunikat = ""
+    dania_stolik = ""
+    for i in range(0,a):
+        tab.append(findMeal(list[i][1]))
+        if(tab[i]!=False):
+            ilosc = list[i][0]
+            global kwota
+            kwota = kwota + int(ilosc)*int((fromMenutoList()[tab[i]-1]).split(";",2)[-1])
+            dania_komunikat += (odmiana(list,i)) + "\n"
+            dania_stolik += (odmiana(list,i)) + "\n"
+        else:
+            danie_blad = "Chcesz zamówić danie, którego nie ma w menu! Proszę zamów coś innego :)"
+            return (danie_blad, "Niestety dania nie ma w naszej karcie.","")
+    print (dania_komunikat, "Przyjąłem zamówienie", dania_stolik)
+    return (dania_komunikat, "Przyjąłem zamówienie. \n", dania_stolik)
+
+
+def odmiana(list, i):  #służy do skomplikowania wyjścia, np. zamówiono 2 serniki ale już dla 5 sztuk mamy zamówiono 5 serników
+        ilosc = list[i][0]
+        inMenu = findMeal(list[i][1])
+        if(ilosc > 4):
+            return(str(ilosc) + menuOdmienionetoArray()[inMenu-1][4])
+        else:
+           return(menuOdmienionetoArray()[inMenu-1][ilosc-1])
+
+def zabierz(list):
+    if list[0][0] == "zabranie":
+        list[0][0] = list[1][0]
+    x = list[0][0]
+    return("Zabrano: " + x + "\n", "Już zabieram. \n", x)
+
+def przyniesF(list):
+    if list[0][0] == "przyniesienie":
+        list[0][0] = list[1][0]
+    x = list[0][0]
+    return("Przyniesiono " + x + "\n", "Proszę bardzo. \n", x)
+
+def odejdz():
+    return("Kelner odchodzi od stołu")
+
+def polec():
+    x = random.randint(1,19)
+    polecam = str(fromMenutoList()[x-1]).split(";",2)[0].lower()
+    return("Polecono: " + polecam, "Dzisiaj polecamy " + polecam + "! \n")
+
+def zaplac(): # funkcja do placenia, oblicza sume zamowienia
+       return("Kwota do zapłaty to " + str(kwota) + " zł.", "Proszę o zapłatę " + str(kwota) + " \n", "Rachunek: " + str(kwota))
 	
 def error(): ### gdy nie zrozumiemy o co chodzi klientowi :)
-	print("Nie zrozumiałem.")
+    return("Nie zrozumiałem.  \n")
 
 ###############################	
 
@@ -156,8 +212,7 @@ def understanding(s):
 		method()
 	return
 
-understanding("zamówić jajecznica tradycyjna omlet owsiany rosół tradycyjny zupa pomidorowa zupa jarzynowa zupa grzybowa żurek staropolski barszcz ukraiński gulasz węgierski leczo klasyczne tatar wołowy bigos po staropolsku sałatka jarzynowa kotlet schabowy naleśniki serowe pierogi ruskie golonka staropolska placki ziemniaczane frytki belgijskie kasza gryczana ziemniaki polskie ryż biały kawa parzona herbata tradycyjna herbata zielona sok jabłkowy woda mineralna sernik na zimno jabłecznik biszkoptowy")
-
+understanding("poproszę dwie sałatki jarzynowe jedną jajecznicę tradycyjną i bigos staropolski")
 ### test
 ##print (understanding("poprosić 2 Zupa pomidorowa 1 Sałatka jarzynowa","V NUMCRD N Adj NUMCRD N Adj"))
 

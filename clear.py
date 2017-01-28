b2ed5ca3731c0ac91b5d623422d3091ca28d0071
wojjar2 <wojjar2@st.amu.edu.pl>
2017-01-28 15:05:12 +0100

    wojciech Zmiany

--- clear.py
+++ clear.py
@@ -26,19 +26,24 @@ def clearbadwords(str = "", file = "badwords.txt"): # pierwszy argument przyjmuj
         f.close()
     print ('Blad')
 
-def wordtoinfinitive(s = '', file = "dictionary.dic" ):
+def wordtoinfinitive(s = '', file = "dicdomyslny.dic" ):
     f = open(file, mode="r+")
+    f_brak = open("brakislownikowe.txt", mode="a")
+    s = clearbadwords(s)
     wordlist = s.split() ## s - podzielone wyrazy stringa zdaniowego - teraz lista
     diclist = f.read().split() ##lista słownika
     resultword = []
     resultcm = []
+    poprzednia = ''
     for word in wordlist:
+        czyznaleziono = 0
         ##tutaj w petli tworzyl bym wyrazenie regularne z kazdego wyrazu
         # pattern = re.compile(word + '(.*)[;](.*)[/]')
         pattern = re.compile(word + '[;].*[/]')
         for tword in diclist:
             wynik = re.match(pattern, tword)
             if wynik != None:
+                czyznaleziono = 1
                 temporary = wynik.group()
                 cm = re.compile('[,].*[/]')
                 czescmowy = re.search(cm, temporary).group().strip(',').strip('/')
@@ -48,16 +53,29 @@ def wordtoinfinitive(s = '', file = "dictionary.dic" ):
                 how_to_inf = re.search(hti, temporary).group().strip(',').strip(';')
                 dl = int(how_to_inf[0])
                 word = word[0:(len(word)-dl)] + (how_to_inf[1:])
+                if (czescmowy =="N" and poprzednia!="NUMCRD"):
+                    resultcm.append('NUMCRD')
+                    resultword.append('1')
+                poprzednia = czescmowy
                 ##print(type(word))
                 ##print(type(czescmowy))
                 resultword.append(word)
                 resultcm.append(czescmowy)
                 break;
+        if(czyznaleziono == 0):
+            f_brak.write(word + "\n")
+            print("Brakuje słowa w słowniku: " + word )
+
+
     res_str = ' '.join(resultword)
     res_cm = ' '.join(resultcm)
     f.close()
     return(res_str,res_cm)
 
 ###test
+##jajecznica tradycyjna omlet owsiany rosół tradycyjny zupa pomidorowa zupa jarzynowa zupa grzybowa żurek staropolski barszcz ukraiński gulasz węgierski leczo klasyczne tatar wołowy bigos po staropolsku sałatka jarzynowa kotlet schabowy naleśniki serowe pierogi ruskie golonka staropolska placki ziemniaczane frytki belgijskie kasza gryczana ziemniaki polskie ryż biały kawa parzona herbata tradycyjna herbata zielona sok jabłkowy woda mineralna sernik na zimno jabłecznik biszkoptowy
 
-print (wordtoinfinitive(clearbadwords("Poproszę o schabowego z frytkami i kapustą kiszoną oraz proszę o znaleźlibyście")))
+
+print (wordtoinfinitive("tatar wołowy dwa bigosy po staropolsku trzy sałatki jarzynowe kotlet schabowy naleśniki serowe pierogi ruskie golonka staropolska placki ziemniaczane frytki belgijskie kasza gryczana ziemniaki polskie ryż biały kawa parzona herbata tradycyjna herbata zielona sok jabłkowy woda mineralna sernik na zimno jabłecznik biszkoptowy"))
+
+

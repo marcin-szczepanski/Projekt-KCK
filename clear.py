600c08e6e3cfb456e4d6fd878f3bcd7d955a077a
wojjar2 <wojjar2@st.amu.edu.pl>
2017-01-29 22:43:58 +0100

    Poprawka

--- clear.py
+++ clear.py
@@ -76,16 +76,13 @@ def wordtoinfinitive(s = '', file = "dicdomyslny.dic" ):
                 how_to_inf = re.search(hti, temporary).group().strip(',').strip(';')
                 dl = int(how_to_inf[0])
                 word = word[0:(len(word)-dl)] + (how_to_inf[1:])
-                if (czescmowy =="N" and poprzednia!="NUMCRD"):
-                    resultcm.append('NUMCRD')
-                    resultword.append('1')
-                ##print(czescmowy)
                 if (poprzednia == "N" and czescmowy !='ADJ'):
-                    print(czescmowy)
-                    print(poprzednia)
                     if (szukajdania(lastword)!=False):
                         resultword.append(szukajdania(lastword).split()[1])
                         resultcm.append('ADJ')
+                if (czescmowy == "N" and poprzednia != "NUMCRD"):
+                    resultcm.append('NUMCRD')
+                    resultword.append('1')
                 lastword = word
                 poprzednia = czescmowy
                 ##print(type(czescmowy))
@@ -95,7 +92,7 @@ def wordtoinfinitive(s = '', file = "dicdomyslny.dic" ):
 
         if(czyznaleziono == 0):
             f_brak.write(word + "\n")
-            ##print("Brakuje słowa w słowniku: " + word )
+            print("Brakuje słowa w słowniku: " + word )
 
     if (czescmowy == "N" and (szukajdania(lastword) != False)):
         resultword.append(szukajdania(word).split()[1])
@@ -110,6 +107,6 @@ def wordtoinfinitive(s = '', file = "dicdomyslny.dic" ):
 ##jajecznica tradycyjna omlet owsiany rosół tradycyjny zupa pomidorowa zupa jarzynowa zupa grzybowa żurek staropolski barszcz ukraiński gulasz węgierski leczo klasyczne tatar wołowy bigos po staropolsku sałatka jarzynowa kotlet schabowy naleśniki serowe pierogi ruskie golonka staropolska placki ziemniaczane frytki belgijskie kasza gryczana ziemniaki polskie ryż biały kawa parzona herbata tradycyjna herbata zielona sok jabłkowy woda mineralna sernik na zimno jabłecznik biszkoptowy
 
 #.,?![]{}();:-_
-#print (wordtoinfinitive("poproszę omlet "))
+#print (wordtoinfinitive("poproszę omlet i żurek staropolski i kotlet"))
 
 #print(szukajdania('menu'))
